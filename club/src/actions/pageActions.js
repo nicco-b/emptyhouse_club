599a5b52d9de53515e99dd3d856e1f46764b29e8
nicco bufalino <niccobeats@icloud.com>
2021-10-18 03:43:18 -0700

    ok

--- club/src/actions/pageActions.js
+++ club/src/actions/pageActions.js
@@ -38,6 +38,7 @@ export const getPage = () => dispatch => {
 // }
 export const getPageById = id => dispatch => {
 	dispatch(setPageLoading())
+	console.log(API_ENDPOINT)
 	axios
 		.get(`${API_ENDPOINT}/api/pages/${id}`)
 		.then(res =>
