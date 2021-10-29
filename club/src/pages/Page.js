5d8fd1cbc441d80fc06fc7c63c072efcfa13de91
nicco bufalino <niccobeats@icloud.com>
2021-10-28 18:31:40 -0700

    faq

--- club/src/pages/Page.js
+++ club/src/pages/Page.js
@@ -4,7 +4,7 @@ import { FaqPage } from './faq'
 
 export const Page = ({ page }) => {
 	if (page.parent === 'pages') {
-		if (page.pageTitle === 'prison') {
+		if (page.pageTitle === 'faq') {
 			return <FaqPage />
 		} else if (page.pageTitle === 'about') {
 			return <AboutPage page={page} />
